008a3c77308c78f52291de3ec4f3766ac45772ac
Bryan Long <BryanLong1@gmail.com>
2018-02-13 15:27:39 -0700

    Routes

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 const queries = require("./queries");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const devMode = process.env.NODE_ENV !== "production";
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (request, response) => {
+app.get("/", (request, response, next) => {
   queries
     .list("highschool_flashcards")
     .then(highschool_flashcards =>
@@ -18,101 +19,122 @@ app.get("/", (request, response) => {
         })
       )
     )
-    .catch(console.error);
+    .catch(next);
 });
 
-app.get("/highschool_flashcards", (request, response) => {
+app.get("/highschool_flashcards", (request, response, next) => {
   queries
     .list("highschool_flashcards")
     .then(highschool_flashcards => {
       response.json({ highschool_flashcards });
     })
-    .catch(console.error);
+    .catch(next);
 });
 
-app.get("/teachers_flashcards", (request, response) => {
+app.get("/teachers_flashcards", (request, response, next) => {
   queries
     .list("teachers_flashcards")
     .then(teachers_flashcards => {
       response.json({ teachers_flashcards });
     })
-    .catch(console.error);
+    .catch(next);
 });
 
-app.get("/highschool_flashcards/:id", (request, response) => {
+app.get("/highschool_flashcards/:id", (request, response, next) => {
   queries
     .read("highschool_flashcards", request.params.id)
     .then(highschool_flashcard => {
       highschool_flashcard ? response.json({ highschool_flashcard }) : response.sendStatus(404);
     })
-    .catch(console.error);
+    .catch(next);
 });
 
-app.get("/teachers_flashcards/:id", (request, response) => {
+app.get("/teachers_flashcards/:id", (request, response, next) => {
   queries
     .read("teachers_flashcards", request.params.id)
     .then(teachers_flashcard => {
       teachers_flashcard ? response.json({ teachers_flashcard }) : response.sendStatus(404);
     })
-    .catch(console.error);
+    .catch(next);
 });
 
-app.post("/highschool_flashcards", (request, response) => {
-  queries
-    .create("highschool_flashcards", request.body)
-    .then(highschool_flashcard => {
-      response.status(201).json({ data: highschool_flashcard });
-    })
-    .catch(console.error);
-});
-
-app.post("/teachers_flashcards", (request, response) => {
+app.post("/teachers_flashcards/", (request, response, next) => {
   queries
     .create("teachers_flashcards", request.body)
     .then(teachers_flashcard => {
       response.status(201).json({ data: teachers_flashcard });
     })
-    .catch(console.error);
+    .catch(next);
 });
 
-app.delete("/highschool_flashcards/:id", (request, response) => {
+app.delete("/teachers_flashcards/:id", (request, response, next) => {
   queries
-    .delete("highschool_flashcards", request.params.id)
-    .then(() => {
-      response.sendStatus(204);
-    })
-    .catch(console.error);
+  .delete("teachers_flashcards", request.params.id)
+  .then(() => {
+    response.sendStatus(204);
+  })
+  .catch(next);
 });
 
-app.delete("/teachers_flashcards/:id", (request, response) => {
+app.put("/teachers_flashcards/:id", (request, response, next) => {
   queries
-    .delete("teachers_flashcards", request.params.id)
-    .then(() => {
-      response.sendStatus(204);
-    })
-    .catch(console.error);
+  .update("teachers_flashcards", request.params.id, request.body)
+  .then(teachers_flashcard => {
+    response.json({ data: teachers_flashcard[0] });
+  })
+  .catch(next);
 });
 
-app.put("/highschool_flashcards/:id", (request, response) => {
-  queries
-    .update("highschool_flashcards", request.params.id, request.body)
-    .then(highschool_flashcard => {
-      response.json({ data: highschool_flashcard[0] });
-    })
-    .catch(console.error);
-});
 
-app.put("/teachers_flashcards/:id", (request, response) => {
-  queries
-    .update("teachers_flashcards", request.params.id, request.body)
-    .then(teachers_flashcard => {
-      response.json({ data: teachers_flashcard[0] });
-    })
-    .catch(console.error);
-});
+// app.post("/highschool_flashcards", (request, response, next) => {
+//   queries
+//     .create("highschool_flashcards", request.body)
+//     .then(highschool_flashcard => {
+  //       response.status(201).json({ data: highschool_flashcard });
+  //     })
+  //     .catch(next);
+  // });
+  
+// app.delete("/highschool_flashcards/:id", (request, response, next) => {
+//   queries
+//     .delete("highschool_flashcards", request.params.id)
+//     .then(() => {
+  //       response.sendStatus(204);
+  //     })
+  //     .catch(next);
+  // });
+  
+// app.put("/highschool_flashcards/:id", (request, response, next) => {
+  //   queries
+  //     .update("highschool_flashcards", request.params.id, request.body)
+  //     .then(highschool_flashcard => {
+    //       response.json({ data: highschool_flashcard[0] });
+    //     })
+    //     .catch(next);
+    // });
+            
+app.use(notFound);
+app.use(errorHandler);
 
-app.use((request, response) => {
-  response.send(404);
-});
+function notFound(req, res, next) {
+  const url = req.originalUrl;
+  if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
+    // Don"t log less important auto requests
+    console.error("[404: Requested file not found] ", url);
+    return res.status(200).send({message: "url path not found"});
+  }
+  res.status(404).send({error: "Url not found", status: 404, url});
+}
+
+function errorHandler(err, req, res, next) {
+  console.error("ERROR", err);
+  const stack =  devMode ? err.stack : undefined;
+  res.status(500).send({
+    error: err.message,
+    url: req.originalUrl,
+    stack,
+  });
+}
 
 module.exports = app;
+            
